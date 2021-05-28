ace25011edf56404c6aef922f1e93f3f1f970078
vivek6899 <vivektrivedi2345vt@gmail.com>
2021-05-28 17:40:45 +0530

    commit

---
+++ Ajax & BAsic form design/modules/Company1.js
@@ -0,0 +1,8 @@
+let company = "Reliance Industry"
+let getCompany = function(){
+   return company.toUpperCase()
+}
+let setCompany = function(newValue){
+   company = newValue
+}
+export {company,getCompany,setCompany}
---
+++ Ajax & BAsic form design/modules/Company2.js
@@ -0,0 +1,7 @@
+import {company,getCompany,setCompany} from './Company1.js'
+console.log(company)
+console.log(getCompany())
+setCompany("Aniket")
+console.log(company)
+console.log(getCompany())
+
